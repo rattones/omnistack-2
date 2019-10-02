af2396708bd98deed025ff03d413e763ffb6cb81
Marcelo Ratton <rattones@marceloratton.com>
2019-10-02 16:07:53 -0300

    fixing and continue class

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -4,8 +4,11 @@ const path = require('path')
 module.exports= {
     storage: multer.diskStorage({
         destination: path.resolve(__dirname, '..', '..', 'uploads'),
-        filename: (req, res, cb) => {
-            cb(null, `${file.filename}-${Date.now()}${path.extname(file.originalname)}`)
+        filename: (req, file, cb) => {
+            const ext = path.extname(file.originalname)
+            const name = path.basename(file.originalname, ext)
+
+            cb(null, `${name}-${Date.now()}${ext}`)
         }
     })
 }
