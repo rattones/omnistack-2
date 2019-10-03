8aba159f89465204504c3d8548d63f92c19f1c46
Marcelo Ratton <rattones@marceloratton.com>
2019-10-03 15:54:27 -0300

    frontend first page designed

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.svg'
+
+function App() {
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-mail *</label>
+          <input type="email" name="email" id="email" placeholder="E-mail de contato"/>
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App;
